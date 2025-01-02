fd767a743ee5cd888988767bf6d9040c5bca79a9
Marcos <m.juniorcsebastiao@gmail.com>
2025-01-02 01:49:35 -0300

    Solves day 1 part 2

--- 01/main.py
+++ 01/main.py
@@ -1,14 +1,35 @@
-sum = 0
+import bisect
 
 with open('input', 'r') as file:
     data = [tuple(map(int, line.split())) for line in file]
 
-sorted_col1 = sorted((num1, idx) for idx, (num1, _) in enumerate(data))
-sorted_col2 = sorted((num2, idx) for idx, (_, num2) in enumerate(data))
+sorted_col1 = sorted([x[0] for x in data])
+sorted_col2 = sorted([x[1] for x in data])
 
-for i in range(len(data)):
-    num1, idx1 = sorted_col1[i]
-    num2, idx2 = sorted_col2[i]
-    sum += abs(num1 - num2)
+def part_1():
+    sum = 0
 
-print(sum)
+    for i in range(len(data)):
+        sum += abs(sorted_col1[i] - sorted_col2[i])
+        
+    print(f"Part 1: {sum}")
+
+def part_2():
+    sum = 0
+    track = 1
+
+    for index, x in enumerate(sorted_col1):
+        if index + 1 < len(sorted_col1) and sorted_col1[index + 1] == x:
+            track += 1
+            continue
+
+        left_index = bisect.bisect_left(sorted_col2, x)
+        right_index = bisect.bisect_right(sorted_col2, x)
+        occurrences = right_index - left_index
+        sum += (track * (occurrences * x))
+        track = 1
+    
+    print(f"Part 2: {sum}")
+
+part_1()
+part_2()
